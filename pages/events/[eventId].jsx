9149e1e193ce218700cf94d941b3c937606bcdb4
omkar-garde <83204526+omkar-garde@users.noreply.github.com>
2023-08-10 14:03:27 +0530

    fixed file structure and added event details page

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,3 +1,29 @@
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import { Fragment } from "react";
+import EventContent from "../../components/event-detail/event-content";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventSummary from "../../components/event-detail/event-summary";
 export default function EventDetailPage() {
-  return <>EventDetailsPage</>;
+  const router = useRouter();
+
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
+  return (
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>{event.description}</EventContent>
+    </Fragment>
+  );
 }
