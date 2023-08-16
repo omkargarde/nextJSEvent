f61ac53a5e9036151fd08b49a2d5388ebae8f5f9
omkargarde <42486951+omkargarde@users.noreply.github.com>
2023-08-16 07:25:03 +0530

    installled swr , homepage not working

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,39 @@
+import useSWR from "swr";
 import EventList from "../components/events/EventList";
 import { getFeaturedEvents } from "../dummy-data";
-
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+export default function HomePage(props) {
+  const featuredEvents = props.featuredEvents;
   return (
     <div>
       <EventList items={featuredEvents}></EventList>
     </div>
   );
+}
+export async function getStaticProps(context) {
+  // const { data, error } = useSWR(
+  //   "https://react-todo-56d15-default-rtdb.asia-southeast1.firebasedatabase.app/events.json",
+  //   (url) => fetch(url).then((res) => res.json())
+  // );
+  // if (error) {
+  //   return {
+  //     props: null,
+  //   };
+  // }
+  const response = await fetch(
+    "https://react-todo-56d15-default-rtdb.asia-southeast1.firebasedatabase.app/events.json"
+  );
+  const data = await response.json();
+
+  const transformedData = [];
+  for (const key in data) {
+    transformedData.push({
+      id: key,
+      ...data[key],
+    });
+  }
+  return {
+    props: {
+      featuredEvents: data,
+    },
+  };
 }
