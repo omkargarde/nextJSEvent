177bd088b77d2f7e9fd73b34019413eda3a134a9
omkargarde <42486951+omkargarde@users.noreply.github.com>
2023-09-05 15:22:23 +0530

    migrated to yarn berry

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,6 @@
 import useSWR from "swr";
 import EventList from "../components/events/EventList";
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-utils";
 export default function HomePage(props) {
   const featuredEvents = props.featuredEvents;
   return (
@@ -10,30 +10,10 @@ export default function HomePage(props) {
   );
 }
 export async function getStaticProps(context) {
-  // const { data, error } = useSWR(
-  //   "https://react-todo-56d15-default-rtdb.asia-southeast1.firebasedatabase.app/events.json",
-  //   (url) => fetch(url).then((res) => res.json())
-  // );
-  // if (error) {
-  //   return {
-  //     props: null,
-  //   };
-  // }
-  const response = await fetch(
-    "https://react-todo-56d15-default-rtdb.asia-southeast1.firebasedatabase.app/events.json"
-  );
-  const data = await response.json();
-
-  const transformedData = [];
-  for (const key in data) {
-    transformedData.push({
-      id: key,
-      ...data[key],
-    });
-  }
+  const featuredEvents = await getFeaturedEvents();
   return {
     props: {
-      featuredEvents: data,
+      featuredEvents: featuredEvents,
     },
   };
 }
